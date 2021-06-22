3c5f3a1c6952cac679ef55e31fe1876a38aa7279
正正 <69309768+HitteField@users.noreply.github.com>
2021-06-22 13:13:42 +0800

    Add files via upload
    客户端相关代码

---
+++ Client/Proto/SysMsg.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MsgPing : MsgBase
+{
+    public MsgPing() { protoName = "MsgPing"; }
+}
+
+public class MsgPong : MsgBase
+{
+    public MsgPong() { protoName = "MsgPong"; }
+}
+
+public class MsgVersionCheck : MsgBase
+{
+    public MsgVersionCheck() { protoName = "MsgVersionCheck"; }
+
+    public string version = "";
+}
